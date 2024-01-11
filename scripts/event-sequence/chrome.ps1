2e61e5a69f5742f120ff87e2921ae7fee3585c00
Gavin <gavinmcguire95@gmail.com>
2024-01-11 12:01:07 -0700

    Pull event sequence

---
+++ scripts/event-sequence/chrome.ps1
@@ -0,0 +1,5 @@
+chrome.exe -remote-debugging-port=9014 --profile-directory="ERISA" --user-data-dir="C:\Documents\Selenium\Test_Chrome\"
+# chrome.exe -remote-debugging-port=9014 --profile-directory=ERISA
+
+# python .\test\test_chrome_getdata.py
+python .\scripts\event-sequence\check-sequence-errors.py
